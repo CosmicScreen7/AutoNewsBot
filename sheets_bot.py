838071e55555e44c3cf7c55796da5e829c546fdc
CosmicScreen7 <CosmicScreen7@users.noreply.github.com>
2026-07-19 23:14:58 +0530

    Clean up models to try and restrict strictly to Imagen 4

--- sheets_bot.py
+++ sheets_bot.py
@@ -258,14 +258,12 @@ def process_new_rows(ws_planning, ws_memory):
                         client = genai.Client(api_key=GEMINI_API_KEY, http_options={'api_version': 'v1alpha'})
                         
                         models_to_try = [
-                            'antigravity',
                             'imagen-4.0-ultra-generate-001',
                             'imagen-4.0-ultra-generate',
                             'imagen-4.0-generate-001',
                             'imagen-4.0-generate',
                             'imagen-4.0-fast-generate-001',
-                            'imagen-4.0-fast-generate',
-                            'imagen-3.0-generate-001'
+                            'imagen-4.0-fast-generate'
                         ]
                         
                         for model_name in models_to_try:
